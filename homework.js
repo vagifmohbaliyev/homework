fbb638b8fb23e0c4041a09b4c15e68b202d2fc65
vaqif mohbaliyev <vagifmohbaliyev45@gmail.com>
2025-11-28 09:45:51 +0400

    homework

---
+++ homework.js
@@ -0,0 +1,45 @@
+// TASK1
+let pet = 'golden retriever';
+result = pet.toUpperCase();
+console.log(result);
+// TASK2
+let question = 'Can you solve this problem?';
+let result2 = question.length;
+console.log(result2);
+// TASK3
+let activity = 'This game is boring';
+let result3 = activity.replace('boring', 'exciting');
+console.log(result3);
+// TASK4
+let headline = 'Choose Your Adventure!';
+let result4 = headline.slice(12, 23);
+console.log(result4);
+// TASK5
+let desserts = ['ice cream', 'pie', 'pudding'];
+desserts.push('cake');
+
+let result5 = desserts;
+console.log(result5);
+// TASK6
+let cities = ['London', 'New york', 'Tokyo'];
+let result6 = cities.includes('Paris');
+console.log(result6);
+// TASK7
+let firstName = ['John', 'Jane'];
+let lastNames = ['Doe', 'Smith'];
+let result7 = firstName.concat(lastNames);
+console.log(result7);
+// TASK8
+let cars = ['Sedan', 'Coupe'];
+let truck = ['Pickup', 'Suv'];
+let result8 = cars.concat(truck);
+console.log(result8);
+// Task 9
+let dessert = ['ice cream', 'pie', 'pudding', 'cake'];
+let result9 = dessert.indexOf('pie');
+console.log(result9);
+// TASK 10
+let word = 'Technology';
+let result10 = word.split('').reverse().join('');
+
+console.log(result10);
